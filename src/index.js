6d24e464e666b697b61aa1213747a8cc8f6ac8d0
anto-10 <anrosaez@gmail.com>
2021-04-10 16:02:23 +0100

    preparé el js y el html para hacer funcional el contact

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import AOS from 'aos';
+import { EmailJSResponseStatus } from 'emailjs-com';
 window.addEventListener("load", function() { AOS.init({initClassName: 'aos-init'}) });
+
+let myform = $("form#contact_form");
+myform.submit(function(event){
+  event.preventDefault();
+
+
+let service_id = "gmail";
+let template_id = "cursos_inscripcion";
+
+myform.find("button").text("Enviando...");
+emailjs.sendForm(service_id,template_id,myform[0])
+  .then(function() {
+      alert("¡Enviado! Nos pondremos en contacto contigo lo antes posible");
+    myform.find("button").text("Enviar");
+  }, function(err) {
+     alert("Upss! Hubo algún problema!\r\n Si persiste, comuníquenos este error:\n" +JASON.stringify(err));
+     myform.find("button").text("Enviar");
+    });
+  return false;
+  });
+
