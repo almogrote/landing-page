8c65f0d9dc80f2da3b4e3a149be954d92d608947
Mathias <yo@mathiasscavello.com>
2021-04-13 20:24:17 +0100

    emailjs working

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import AOS from 'aos';
-import emailjs from 'emailjs-com';
-window.addEventListener("load", function () { AOS.init({ initClassName: 'aos-init' }) });
+import AOS from "aos";
+import emailjs from "emailjs-com";
+
+window.addEventListener("load", function() { AOS.init({ initClassName: "aos-init" }); });
 
 emailjs.init("user_7YPc1Yuylkz5sWQM1uWbB");
+const myform = document.querySelector("form#contact_form");
+const serviceId = "gmail2";
+const templateId = "template_w2zrmji";
 
-let myform = document.querySelector("form#contact_form");
-myform.addEventListener("submit", function (event) {
+myform.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log(event.target);
-
-
-  let service_id = "gmail2";
-  let template_id = "template_w2zrmji";
-
-  emailjs.sendForm(service_id, template_id, myform[0])
-    .then(function () {
+  emailjs.sendForm(serviceId, templateId, myform)
+    .then(function() {
       alert("¡Enviado! Nos pondremos en contacto contigo lo antes posible");
-    }, function (err) {
-      alert("Upss! Hubo algún problema!\r\n Si persiste, comuníquenos este error:\n" + JASON.stringify(err));
+    }, function(err) {
+      alert("Upss! Hubo algún problema!\r\n Si persiste, comuníquenos este error:\n" + JSON.stringify(err));
     });
   return false;
 });
